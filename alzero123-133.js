b2a8d7fdb0f81b790699eda50da8569c61120854
Duniaalkilany <duniaalkilany908@gmail.com>
2022-01-02 11:57:12 +0200

    123-133

--- alzero123-133.js
+++ alzero123-133.js
@@ -744,3 +744,32 @@ let objTwo = {
 let allObjs={...objOne ,...objTwo,e:5}
 console.log(allObjs);
 console.log({...objOne ,...objTwo});
+
+//=======================================video133==================================================//
+
+
+/*
+  Map And Set + What You Learn => Challenge
+  Requirements
+  - You Cant Use Numbers Or True Or False
+  - Don't Use Array Indexes
+  - You Cant Use Loop
+  - You Cant Use Any Higher Order Function
+  - Only One Line Solution Inside Console
+  - If You Use Length => Then Only Time Only
+  Hints
+  - You Can Use * Operator Only In Calculation
+  - Set
+  - Spread Operator
+  - Math Object Methods
+*/
+let n1 = [10, 30, 10, 20];
+let n2 = [30, 20, 10];
+
+console.log("Your Solution Here"); // 210
+
+//spread 
+
+console.log([...n1,...n2].length*Math.max(...new Set([...n1,...n2])));//[10, 30, 20] 
+console.log([...n1,...n2].length*Math.max(...n1));
+console.log([...n1,...n2].length*Math.max(...new Set([...n1])));
--- challenges123-133.js
+++ challenges123-133.js
@@ -14,15 +14,142 @@ console.log(setOfNumbers);//object{10}
 //add 20 
 setOfNumbers.add(20).add(setOfNumbers.size)
 console.log(setOfNumbers);//object{10,20,2}
+console.log(Array.from(setOfNumbers)[setOfNumbers.size-1]);
+console.log([...setOfNumbers][2]);
 
-//access the set 
-//Set     => Have Keys, Values, Entries
-//keys(),values()===>methods to have set iterator===>iterate through the set 
-// console.log(setOfNumbers.keys().next().value);
+//challenge 2
+let myFriends = ["Osama", "Ahmed", "Sayed", "Sayed", "Mahmoud", "Osama"];
+console.log([...new Set(myFriends)].sort());
+// Needed Output
+// (4) ['Ahmed', 'Mahmoud', 'Osama', 'Sayed']
 
-//i can use gorEach with Set
 
-setOfNumbers.forEach((ele)=>{
-    
-setOfNumbers.keys().next().value ==undefined?console.log(ele):console.log("vvv");
-})
+console.log(Array.from(new Set(myFriends)).sort());
+
+
+//challenge 3
+let myInfo = {
+    username: "Osama",
+    role: "Admin",
+    country: "Egypt",
+  };
+let stringKeys=Object.keys(myInfo)//['username','role','country']
+let objValues=Object.values(myInfo)
+let myMap=new Map([[stringKeys[0],objValues[0]],[stringKeys[1],objValues[1]],[stringKeys[2],objValues[2]]]);
+  
+ console.log(myMap)
+ console.log(myMap.size);
+ console.log(myMap.has("role"));
+  // Needed Output
+//   Map(3) {'username' => 'Osama', 'role' => 'Admin', 'country' => 'Egypt'}
+//   3
+//   true
+
+
+//
+console.log(Array.isArray(Object.entries(myInfo) ) );
+console.log(Object.entries(myInfo));//[[key,value],[],[]]//array of arrays 
+
+myMap2=new Map(Object.entries(myInfo))
+console.log(myMap2);
+//challenge 4
+let theNumber = 100020003000;
+
+// Needed Output
+// 123
+
+let stringIte=theNumber.toString()
+console.log(Number([...new Set(theNumber.toString())].sort().join("")));//Number("0123")===>123
+// console.log([...new Set([...theNumber.toString()])]);
+
+//challenge 5
+
+let theName = "Elzero";
+
+// Needed Output
+// ['E', 'l', 'z', 'e', 'r', 'o']
+
+console.log([...theName]);
+console.log(Array.from(theName));
+console.log(theName.split(""));
+let arr=[]
+for(i=0;i<theName.length;i++){
+    arr.push(theName[i])
+}
+console.log(arr);
+
+console.log([...new Set(theName)]);
+
+
+console.log(Object.values(theName));
+
+let name="dunia"
+console.log(Object.values(name));//['d', 'u', 'n', 'i', 'a']
+console.log(Object.keys(name));//['0', '1', '2', '3', '4']
+///i can assume that string is object 
+
+
+//challenge 6
+
+// let chars = ["A", "B", "C", "D", "E", 10, 15, 6];
+let chars = ["A", "B", "C", 20, "D", "E", 10, 15, 6];
+// let chars = ["Z", "Y", "A", "D", "E", 10, 1];
+//lopp through array ===>ele number count + 
+//for loop //type number [array of numbers]
+//concat 
+//.coppyWithin(0,)
+
+let removed=[]
+
+    for(let i=0;i<chars.length;i++){
+      
+        if (typeof chars[i]==="number"){
+          console.log(chars[i]);
+          console.log(i);
+    removed=chars.splice(i,1)//array updated//also i have removed elements returned 
+
+console.log(removed);
+        }
+    }
+
+    let conc=[...removed,...chars]
+    console.log('firstttt',conc);
+  for (let i=0;i<conc.length;i++){
+      if(typeof conc[i]==="string"){
+         start=i
+         break
+      }
+  }
+  console.log(start);    
+
+    console.log(conc);
+    conc.copyWithin(0,start,start+removed.length)
+    console.log(conc)
+// console.log(chars);
+
+//challenge7
+let numsOne = [1, 2, 3];
+let numsTwo = [4, 5, 6];
+
+// Needed Output
+// [1, 2, 3, 4, 5, 6]
+
+// numsOne=numsOne.concat(numsTwo)
+// console.log(numsOne);
+// console.log([...numsOne,...numsTwo]);
+
+// numsOne.push(...numsTwo);
+
+// numsTwo.unshift(...numsOne);
+
+// console.log(Array.from([...numsOne,...numsTwo]));
+// console.log(Array.from(new Set([...numsOne,...numsTwo])));
+
+// numsOne[numsOne.length]=numsTwo
+// console.log(numsOne);
+// numsOne=numsOne.flat()
+// console.log(numsOne);
+
+console.log(Array.from([numsOne,numsTwo]).flat());
+
+console.log(Array.of(...numsOne,...numsTwo));
